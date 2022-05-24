1f04c6f9e9bb685719329f0e8a8473ec0210cf34
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2022-05-24 14:40:55 +0800

    added PointQuery Api (api endpoint for lat-long getter())

--- sqs/components/masterlist/api.py
+++ sqs/components/masterlist/api.py
@@ -17,7 +17,7 @@ import traceback
 import json
 
 from sqs.components.masterlist.models import Layer, LayerHistory, Feature
-from sqs.utils.geoquery_utils import GeoQueryHelper
+from sqs.utils.geoquery_utils import LayerQueryHelper, PointQueryHelper
 from sqs.components.masterlist.serializers import GeoTestSerializer, LayerSerializer, FeatureGeometrySerializer
 
 import logging
@@ -58,30 +58,51 @@ class LayerViewSet(viewsets.ReadOnlyModelViewSet):
         seture_featuresrializer = self.get_serializer(instance) 
         return Response(serializer.data)
 
-    @action(detail=False, methods=['POST',]) # POST because request will contain GeoJSON polygon to intersect with layer stored on SQS
+    @action(detail=False, methods=['POST',]) # POST because request will contain GeoJSON polygon to intersect with layer stored on SQS. If layer does not exist, SQS will retrieve from KMI
     def spatial_query(self, request, *args, **kwargs):            
         """ 
-        http://localhost:8002/api/layers/intersect.json 
+        http://localhost:8002/api/layers/spatial_query.json 
 
-        curl -d @sqs/data/json/goldfields_curl_query.json -X POST http://localhost:8002/api/layers/intersect.json --header "Content-Type: application/json" --header "Accept: application/json"
+        curl -d @sqs/data/json/goldfields_curl_query.json -X POST http://localhost:8002/api/layers/spatial_query.json --header "Content-Type: application/json" --header "Accept: application/json"
 
         or
-        curl -d '{"names":["office","region"],"geojson":{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[124.12353515624999,-30.391830328088137],[124.03564453125,-31.672083485607377],[126.69433593749999,-31.615965936476076],[127.17773437499999,-29.688052749856787],[124.12353515624999,-30.391830328088137]]]}}]}}' -X POST http://localhost:8002/api/layers/intersect.json --header "Content-Type: application/json"
+        curl -d '{"names":["office","region"],"geojson":{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[124.12353515624999,-30.391830328088137],[124.03564453125,-31.672083485607377],[126.69433593749999,-31.615965936476076],[127.17773437499999,-29.688052749856787],[124.12353515624999,-30.391830328088137]]]}}]}}' -X POST http://localhost:8002/api/layers/spatial_query.json --header "Content-Type: application/json"
 
         """
-        import ipdb; ipdb.set_trace()
-        names = request.data['layers'][0]['names']
-        type_name = request.data['layers'][0]['type_name']
-
+        layer_data = request.data['layers']
         geojson = request.data['geojson']
-        #layer = Layer.objects.filter(type=type_name.split(':')[0], name=type_name.split(':')[1])
-        layer = self.queryset.get(type_name=type_name)
 
-
-        helper=GeoQueryHelper(layer)
+        import ipdb; ipdb.set_trace()
+        helper = LayerQueryHelper(layer_data, geojson)
         #res = helper.intersection(['region', 'office'], request.data)
-        res = helper.intersection(names, geojson)
+        res = helper.spatial_join()
         return Response(res)
+
+    @action(detail=False, methods=['POST',]) # POST because request will contain GeoJSON polygon to intersect with layer stored on SQS. If layer does not exist, SQS will retrieve from KMI
+    def point_query(self, request, *args, **kwargs):            
+        """ 
+        http://localhost:8002/api/layers/point_query.json
+
+        curl -d '{"layer_name": "cddp:dpaw_regions", "layer_attrs":["office","region"], "longitude": 121.465836, "latitude":-30.748890}' -X POST http://localhost:8002/api/layers/point_query.json --header "Content-Type: application/json" --header "Accept: application/json"
+        """
+        try:
+            layer_name = request.data['layer_name']
+            longitude = request.data['longitude']
+            latitude = request.data['latitude']
+            layer_attrs = request.data.get('layer_attrs', [])
+            predicate = request.data.get('predicate', 'within')
+
+            #import ipdb; ipdb.set_trace()
+            helper = PointQueryHelper(layer_name, layer_attrs, longitude, latitude)
+            res = helper.spatial_join(predicate=predicate)
+            return Response(res)
+        except KeyError as e:
+            #return Response(repr(e))
+            raise repr(e)
+        except Exception as e:
+            #return Response(str(e))
+            raise str(e)
+
 
 
 
@@ -99,27 +120,27 @@ class FeatureViewSet(viewsets.ReadOnlyModelViewSet):
         serializer = FeatureGeometrySerializer(qs,context={'request':request}, many=True)
         return Response(serializer.data)
 
-    @action(detail=True, methods=['POST',]) # POST because request will contain GeoJSON polygon to intersect with layer stored on SQS
-    def intersect(self, request, *args, **kwargs):            
-        """ 
-        http://localhost:8002/api/layer_features/50/intersect.json 
-
-        curl -d @sqs/data/json/goldfields_curl_query.json -X POST http://localhost:8002/api/layer_features/50/intersect.json --header "Content-Type: application/json" --header "Accept: application/json"
-        or
-        curl -d '{"names":["office","region"],"geojson":{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[124.12353515624999,-30.391830328088137],[124.03564453125,-31.672083485607377],[126.69433593749999,-31.615965936476076],[127.17773437499999,-29.688052749856787],[124.12353515624999,-30.391830328088137]]]}}]}}' -X POST http://localhost:8002/api/layer_features/5/intersect.json --header "Content-Type: application/json"
-
-        """
-        import ipdb; ipdb.set_trace()
-        names = request.data['names']
-        geojson = request.data['geojson']
-        type_name = request.data['type_name']
-        layer = self.get_object()
-
-
-        helper=GeoQueryHelper(layer)
-        #res = helper.intersection(['region', 'office'], request.data)
-        res = helper.intersection(names, geojson)
-        return Response(res)
+#    @action(detail=True, methods=['POST',]) # POST because request will contain GeoJSON polygon to intersect with layer stored on SQS
+#    def intersect(self, request, *args, **kwargs):            
+#        """ 
+#        http://localhost:8002/api/layer_features/50/intersect.json 
+#
+#        curl -d @sqs/data/json/goldfields_curl_query.json -X POST http://localhost:8002/api/layer_features/50/intersect.json --header "Content-Type: application/json" --header "Accept: application/json"
+#        or
+#        curl -d '{"names":["office","region"],"geojson":{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[124.12353515624999,-30.391830328088137],[124.03564453125,-31.672083485607377],[126.69433593749999,-31.615965936476076],[127.17773437499999,-29.688052749856787],[124.12353515624999,-30.391830328088137]]]}}]}}' -X POST http://localhost:8002/api/layer_features/5/intersect.json --header "Content-Type: application/json"
+#
+#        """
+#        import ipdb; ipdb.set_trace()
+#        names = request.data['names']
+#        geojson = request.data['geojson']
+#        type_name = request.data['type_name']
+#        layer = self.get_object()
+#
+#
+#        helper=GeoQueryHelper(layer)
+#        #res = helper.intersection(['region', 'office'], request.data)
+#        res = helper.intersection(names, geojson)
+#        return Response(res)
 
 
 
--- sqs/utils/geoquery_utils.py
+++ sqs/utils/geoquery_utils.py
@@ -2,6 +2,7 @@ from django.contrib.gis.geos import GEOSGeometry, Polygon, MultiPolygon
 from django.conf import settings
 from django.db import transaction
 
+import pandas as pd
 import geopandas as gpd
 import requests
 import json
@@ -14,59 +15,149 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class GeoQueryHelper():
+#class GeoQueryHelper():
+#
+#    def __init__(self, layer):
+#        self.layer = layer if isinstance(layer, Layer) else gdf.read_file(layer)
+#
+#    def filter_dict(self, feature, required_attributes):
+#        return dict((key,value) for key, value in feature.attributes.items() if key in required_attributes)
+#
+#    def intersection_v1(self, required_attributes, polygon_geojson=None):
+#
+#        #with open('sqs/data/json/south_wa.json') as f:
+#        if not polygon_geojson:
+#            with open('sqs/data/json/goldfields.json') as f:
+#                polygon_geojson = json.load(f)
+#
+#        intersection_geom = []
+#        for ft in polygon_geojson['features']:
+#            geom_str = json.dumps(ft['geometry'])
+#            geom = GEOSGeometry(geom_str)
+#            try:
+#                if isinstance(geom, MultiPolygon):
+#                    continue
+#
+#                elif isinstance(geom, Polygon):
+#                    geom = MultiPolygon([geom])
+#             
+#                    #for region in DpawRegion.objects.filter(geom__intersects=geom):
+#                    #import ipdb; ipdb.set_trace()
+#                    for feature in self.layer.features.filter(geometry__intersects=geom):
+#                        #intersection_geom.append(geom.intersection(feature.geometry))
+#                        intersection_geom.append(self.filter_dict(feature, required_attributes))
+#                        #print(feature)
+#                    #import ipdb; ipdb.set_trace()
+#                print(intersection_geom)
+#            except TypeError as e:
+#                print(e)
+#        return intersection_geom
+#
+#    def intersection(self, required_attributes, mpoly=None):
+#
+#        import ipdb; ipdb.set_trace()
+#        if not mpoly:
+#            mpoly = gpd.read_file('sqs/data/json/goldfields.json')
+#        else:
+#            mpoly = gpd.read_file(json.dumps(mpoly))
+#
+#        # 
+#        if mpoly.crs.srs != 'EPSG:4236':
+#            mpoly.to_crs('EPSG:4236', inplace=True)
+#
+#        layer_gdf = self.layer.layer_to_gdf
+#        layer_gdf.to_crs(mpoly.crs, inplace=True)
+#        overlay = layer_gdf.overlay(mpoly, how='intersection')
+#
+#        return overlay[required_attributes].to_json()
 
-    def __init__(self, layer):
-        self.layer = layer if isinstance(layer, Layer) else gdf.read_file(layer)
 
-    def filter_dict(self, feature, required_attributes):
-        return dict((key,value) for key, value in feature.attributes.items() if key in required_attributes)
+class LayerQueryHelper():
 
-    def intersection_v1(self, required_attributes, polygon_geojson=None):
+    def __init__(self, layer_data, geojson):
+        #self.layer = layer if isinstance(layer, Layer) else gdf.read_file(layer)
+        self.layer_data = layer_data
+        self.geojson = self.read_geojson(geojson)
 
-        #with open('sqs/data/json/south_wa.json') as f:
-        if not polygon_geojson:
-            with open('sqs/data/json/goldfields.json') as f:
-                polygon_geojson = json.load(f)
+    def read_geojson(self, geojson):
+        """ geojson is the use specified polygon, used to intersect the layers """
+        mpoly = gpd.read_file(json.dumps(geojson))
+        if mpoly.crs.srs != settings.CRS:
+            # CRS = 'EPSG:4236'
+            mpoly.to_crs(settings.CRS, inplace=True)
 
-        intersection_geom = []
-        for ft in polygon_geojson['features']:
-            geom_str = json.dumps(ft['geometry'])
-            geom = GEOSGeometry(geom_str)
-            try:
-                if isinstance(geom, MultiPolygon):
-                    continue
+        return mpoly
 
-                elif isinstance(geom, Polygon):
-                    geom = MultiPolygon([geom])
-             
-                    #for region in DpawRegion.objects.filter(geom__intersects=geom):
-                    #import ipdb; ipdb.set_trace()
-                    for feature in self.layer.features.filter(geometry__intersects=geom):
-                        #intersection_geom.append(geom.intersection(feature.geometry))
-                        intersection_geom.append(self.filter_dict(feature, required_attributes))
-                        #print(feature)
-                    #import ipdb; ipdb.set_trace()
-                print(intersection_geom)
-            except TypeError as e:
-                print(e)
-        return intersection_geom
-
-    def intersection(self, required_attributes, mpoly=None):
+    def spatial_join(self):
 
         import ipdb; ipdb.set_trace()
-        if not mpoly:
-            mpoly = gpd.read_file('sqs/data/json/goldfields.json')
-        else:
-            mpoly = gpd.read_file(json.dumps(mpoly))
 
-        # 
-        if mpoly.crs.srs != 'EPSG:4236':
-            mpoly.to_crs('EPSG:4236', inplace=True)
+        res = [] 
+        for data in self.layer_data:
+            
+            column_names = data['names']
+            type_name = data['type_name']
 
-        layer_gdf = self.layer.layer_to_gdf
-        layer_gdf.to_crs(mpoly.crs, inplace=True)
-        overlay = layer_gdf.overlay(mpoly, how='intersection')
+            layer = Layer.objects.get(type_name=type_name, current=True)
+            layer_gdf = layer.layer_to_gdf
+            if layer_gdf.crs.srs != settings.CRS:
+                layer_gdf.to_crs(settings.CRS, inplace=True)
 
-        return overlay[required_attributes].to_json()
+            overlay_res = layer_gdf.overlay(self.geojson, how='intersection')
+            res.append(
+                dict(type_name=type_name, res=overlay_res[column_names].to_json())
+            )
+
+        return res
+
+
+class PointQueryHelper():
+    """
+    pq = PointQueryHelper('cddp:dpaw_regions', ['region','office'], 121.465836, -30.748890)
+    pq.spatial_join()
+    """
+
+    def __init__(self, layer_name, layer_attrs, longitude, latitude):
+        self.layer_name = layer_name
+        self.layer_attrs = layer_attrs
+        self.longitude = longitude
+        self.latitude = latitude
+
+    def spatial_join(self, predicate='within'):
+
+        #import ipdb; ipdb.set_trace()
+        layer = Layer.objects.get(type_name=self.layer_name, current=True)
+        layer_gdf = layer.layer_to_gdf
+
+        # Lat Long for Kalgoolie, Goldfields
+        # df = pd.DataFrame({'longitude': [121.465836], 'latitude': [-30.748890]})
+        # settings.CRS = 'EPSG:4236'
+        df = pd.DataFrame({'longitude': [self.longitude], 'latitude': [self.latitude]})
+        point_gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs=settings.CRS)
+
+        overlay_res = gpd.sjoin(point_gdf, layer_gdf, predicate=predicate)
+
+        attrs_exist = all(item in overlay_res.columns for item in self.layer_attrs)
+        errors = None
+        if len(self.layer_attrs)==0 or overlay_res.empty:
+            # no attrs specified - so return them all
+            layer_attrs = overlay_res.drop('geometry', axis=1).columns
+        elif len(self.layer_attrs)>0 and attrs_exist:
+            # return only requested attrs
+            layer_attrs = self.layer_attrs 
+        else: #elif not attrs_exist:
+            # one or more attr requested not found in layer - return all attrs and error message
+            layer_attrs = overlay_res.drop('geometry', axis=1).columns
+            errors = f'Attribute(s) not available: {self.layer_attrs}. Attributes available in layer: {list(layer_attrs.array)}'
+
+        #layer_attrs = self.layer_attrs if len(self.layer_attrs)>0 and attrs_exist else overlay_res.drop('geometry', axis=1).columns
+        overlay_res = overlay_res.iloc[0] if not overlay_res.empty else overlay_res # convert row to pandas Series (removes index)
+
+        try: 
+            res = dict(type_name=self.layer_name, errors=errors, res=overlay_res[layer_attrs].to_dict() if not overlay_res.empty else None)
+        except Exception as e:
+            logger.error(e)
+            res = dict(type_name=self.layer_name, error=str(e), res=overlay_res.to_dict() if not overlay_res.empty else None)
+
+        return res
 
